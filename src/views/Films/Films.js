49160c355e8354e321258e6cec9eebb8e0e02504
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-03-27 19:54:46 -0700

    created a Film function and set state

---
+++ src/views/Films/Films.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { fetchFilms } from '../../services/films';
+
+export default function Films() {
+  const [films, setFilms] = useState([]);
+}
