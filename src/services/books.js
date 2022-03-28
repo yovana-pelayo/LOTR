254128b7bc112c603453c639c742693ebf70d4f2
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-03-27 20:50:36 -0700

    book titles are displayed on page,

--- src/services/books.js
+++ src/services/books.js
@@ -1,5 +1,8 @@
 export async function fetchBooks() {
-  const resp = await fetch(`${process.env.REACT_APP_SUPABASE_URL}/rest/v1/books?`, {
+  const params = new URLSearchParams();
+  params.set('select', '*');
+  let url = `${process.env.REACT_APP_SUPABASE_URL}/rest/v1/books?${params.toString()}`;
+  const resp = await fetch(url, {
     headers: {
       apikey: process.env.REACT_APP_SUPABASE_KEY,
       Authorization: `Bearer ${process.env.REACT_APP_SUPABASE_KEY}`,
--- src/views/Books/Books.js
+++ src/views/Books/Books.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom/cjs/react-router-dom.min';
 import { fetchBooks } from '../../services/books';
 
 export default function Books() {
+  const params = useParams();
   const [books, setBooks] = useState([]);
   const [loading, setLoading] = useState(true);
   useEffect(() => {
@@ -11,8 +13,17 @@ export default function Books() {
       setLoading(false);
     };
     fetchData();
-  }, []);
+  }, [params]);
 
   if (loading) return <div>Loading</div>;
-  return <div>Books</div>;
+  return (
+    <div>
+      <h1>Books</h1>
+      {books.map((book) => (
+        <ul key={book.id}>
+          <li>{book.title}</li>
+        </ul>
+      ))}
+    </div>
+  );
 }
